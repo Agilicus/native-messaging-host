29c5d70a132e427c94df92603c29501f2876dd3e
Don Bowman <don@agilicus.com>
2023-06-24 13:26:27 -0400

    fix: allow multiple versions of Google Chrome (e.g. beta) on darwin

--- go.mod
+++ go.mod
@@ -5,6 +5,12 @@ go 1.20
 require (
 	github.com/google/go-cmp v0.5.9
 	github.com/hashicorp/go-version v1.6.0
-	github.com/stretchr/testify v1.8.2 // indirect
+	github.com/stretchr/testify v1.8.2
 	golang.org/x/sys v0.7.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- manifest_darwin.go
+++ manifest_darwin.go
@@ -18,15 +18,20 @@ import (
 // location for OS X.
 //
 // See https://developer.chrome.com/extensions/nativeMessaging#native-messaging-host-location-nix
-func (h *Host) getTargetName() string {
-	target := "/Library/Google/Chrome/NativeMessagingHosts"
-
+func (h *Host) getTargetNames() ([]string, error) {
+	targets := []string{}
 	if os.Getuid() != 0 {
 		homeDir, _ := os.UserHomeDir()
-		target = homeDir + "/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+		matches, err := filepath.Glob(homeDir + "/Library/Application Support/Google/Chrome/NativeMessagingHosts")
+		if err != nil {
+			return nil, err
+		}
+		for _, s := range matches {
+			target := filepath.Join(s, h.AppName+".json")
+			targets = append(targets, target)
+		}
 	}
-
-	return filepath.Join(target, h.AppName+".json")
+	return targets, nil
 }
 
 // Install creates native-messaging manifest file on appropriate location. It
@@ -35,17 +40,22 @@ func (h *Host) getTargetName() string {
 // See https://developer.chrome.com/extensions/nativeMessaging#native-messaging-host-location-nix
 func (h *Host) Install() error {
 	manifest, _ := json.MarshalIndent(h, "", "  ")
-	targetName := h.getTargetName()
-
-	if err := osMkdirAll(filepath.Dir(targetName), 0755); err != nil {
+	targetNames, err := h.getTargetNames()
+	if err != nil {
 		return err
 	}
 
-	if err := ioutilWriteFile(targetName, manifest, 0644); err != nil {
-		return err
-	}
+	for _, targetName := range targetNames {
+		if err := osMkdirAll(filepath.Dir(targetName), 0755); err != nil {
+			return err
+		}
 
-	log.Printf("Installed: %s", targetName)
+		if err := ioutilWriteFile(targetName, manifest, 0644); err != nil {
+			return err
+		}
+
+		log.Printf("Installed: %s", targetName)
+	}
 	return nil
 }
 
@@ -53,24 +63,30 @@ func (h *Host) Install() error {
 //
 // See https://developer.chrome.com/extensions/nativeMessaging#native-messaging-host-location-nix
 func (h *Host) Uninstall() {
-	targetName := h.getTargetName()
+	targetNames, err := h.getTargetNames()
 
-	if err := os.Remove(targetName); err != nil {
-		// It might never have been installed.
-		log.Print(err)
+	if err != nil {
+		return
 	}
 
-	if err := os.Remove(h.ExecName); err != nil {
-		// It might be locked by current process.
-		log.Print(err)
-	}
+	for _, targetName := range targetNames {
+		if err := os.Remove(targetName); err != nil {
+			// It might never have been installed.
+			log.Print(err)
+		}
 
-	if err := os.Remove(h.ExecName + ".chk"); err != nil {
-		// It might not exist.
-		log.Print(err)
-	}
+		if err := os.Remove(h.ExecName); err != nil {
+			// It might be locked by current process.
+			log.Print(err)
+		}
 
-	log.Printf("Uninstalled: %s", targetName)
+		if err := os.Remove(h.ExecName + ".chk"); err != nil {
+			// It might not exist.
+			log.Print(err)
+		}
+
+		log.Printf("Uninstalled: %s", targetName)
+	}
 
 	// Exit gracefully.
 	runtimeGoexit()
